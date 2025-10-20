e4879b14191703fb6fded99c5d5d09d61ed85729
naza0189 <bjjb0189@leeds.ac.uk>
2025-10-20 21:26:25 +0100

    Finished base model, will add colour functions after.

--- portfolio/wordle/src/main/kotlin/Main.kt
+++ portfolio/wordle/src/main/kotlin/Main.kt
@@ -1,2 +1,26 @@
 fun main() {
+    val wordList = readWordList("C:\\Users\\nazaa\\IdeaProjects\\comp2850-oop-work\\portfolio\\wordle\\data\\words.txt")
+    val selectedWord = pickRandomWord(wordList)
+    println(selectedWord)
+    println("Welcome to Wordle!\nGuess today's word!") //intro message
+    for (i in 1..10){
+        val userGuess = obtainGuess(GuessCounter.guessCounter, wordList)
+        if (!isValid(userGuess, wordList)){
+            continue
+        }
+        val evaluatedGuess = evaluateGuess(userGuess, selectedWord)
+        val correct = displayGuess(userGuess, evaluatedGuess)
+        if (correct){
+            println("You guessed it correctly!")
+            break
+        }
+    }
+    println("Better luck next time!")
+
 }
+//get random word
+//get guess
+//compare guess
+//display comparison
+//if false, repeat loop
+//if true congratulate the user and end program
--- portfolio/wordle/src/main/kotlin/Wordle.kt
+++ portfolio/wordle/src/main/kotlin/Wordle.kt
@@ -1,1 +1,54 @@
+import java.io.File
 // Implement the six required functions here
+fun isValid(word: String, wordList: MutableList<String>): Boolean{
+    if (word.length != 5) { //checking whether the word is 5 letters
+        println("Word must be 5 letters")
+        return false
+    } else if (word !in wordList){ //checking whether the guess is included in the list
+        println("Invalid word")
+        return false
+    } else { //adding the guess counter by one if the word
+        GuessCounter.guessCounter = GuessCounter.guessCounter + 1
+        return true
+    }
+}
+fun readWordList(filename: String): MutableList<String>{
+    val wordList = File(filename).bufferedReader().readLines().toMutableList()
+    return wordList
+}
+fun pickRandomWord(wordList: MutableList<String>): String{
+    val selectedWord = wordList.random()
+    return selectedWord
+}
+fun obtainGuess(attempt: Int, wordList: MutableList<String>): String{
+    println("Guess no: ${GuessCounter.guessCounter.toString()}\n")
+    val userGuess = readln().uppercase()
+    return userGuess.toString()
+}
+
+fun evaluateGuess(guess: String, target: String): List<Int>{
+    val guessWordGuesser = mutableListOf<Int>()
+    for (i in guess.indices){
+        if (guess[i] == target[i]){
+            guessWordGuesser.add(2)
+        }else if (guess[i] in target){
+            guessWordGuesser.add(1)
+        }else {
+            guessWordGuesser.add(0)
+        }
+    }
+    return guessWordGuesser
+}
+fun displayGuess(guess: String, matches: List<Int>): Boolean{
+    println("Your guess: $guess\nCorrect guesses: $matches")
+    if (matches == listOf(2, 2, 2, 2, 2)){
+        return true
+    }else{
+        return false
+    }
+}
+class GuessCounter{
+    companion object{
+        var guessCounter: Int = 1
+    }
+}
