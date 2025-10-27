5e2269b0f2b55858f71dcacdce467a9e448dd68a
naza0189 <bjjb0189@leeds.ac.uk>
2025-10-27 16:44:37 +0000

    Done Task 3.

--- tasks/task3_1/Args.kt
+++ tasks/task3_1/Args.kt
@@ -1,1 +1,11 @@
 // Task 3.1: command line arguments
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    if (args.size != 1) {
+        println("Error: filename required as sole argument")
+        exitProcess(1)
+    }
+    println(args[0])
+    println(args[1])
+}
